0cc93f400b1fe798fa14d14f667df3745158bc36
Chad Harrington <charrington@datascaler.com>
2011-01-18 17:20:54 -0800

    Initial import

---
+++ chad.el
@@ -0,0 +1,99 @@
+;; Make the cursor a black bar
+(setq default-cursor-type 'bar)
+(set-cursor-color "black")
+
+;; Mac-specific stuff
+(setq browse-url-browser-function 'browse-url-default-macosx-browser
+      delete-by-moving-to-trash t)
+(add-to-list 'ido-ignore-files "\\.DS_Store")
+;; Set meta to be the command key
+(setq mac-option-key-is-meta nil)
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
+
+;; use UTF-8
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+(column-number-mode 1)
+(normal-erase-is-backspace-mode 1)
+
+(setq c-mode-hook
+    (function (lambda ()
+                (setq indent-tabs-mode nil)
+                (setq c-indent-level 4))))
+(setq objc-mode-hook
+    (function (lambda ()
+                (setq indent-tabs-mode nil)
+                (setq c-indent-level 4))))
+(setq c++-mode-hook
+    (function (lambda ()
+                (setq indent-tabs-mode nil)
+                (setq c-indent-level 4))))
+
+(setq whitespace-style '(trailing lines space-before-tab
+                          indentation space-after-tab)
+      whitespace-line-column 80)
+
+;; rst.el
+(require 'rst)
+(setq auto-mode-alist
+      (append '(("\\.rst$" . rst-mode)
+                ("\\.rest$" . rst-mode)) auto-mode-alist))
+
+;; Clojure repl syntax highlighting (http://gist.github.com/337280)
+;;; all code in this function lifted from the clojure-mode function
+;;; from clojure-mode.el
+(defun clojure-font-lock-setup ()
+  (interactive)
+  (set (make-local-variable 'lisp-indent-function)
+       'clojure-indent-function)
+  (set (make-local-variable 'lisp-doc-string-elt-property)
+       'clojure-doc-string-elt)
+  (set (make-local-variable 'font-lock-multiline) t)
+ 
+  (add-to-list 'font-lock-extend-region-functions
+               'clojure-font-lock-extend-region-def t)
+ 
+  (when clojure-mode-font-lock-comment-sexp
+    (add-to-list 'font-lock-extend-region-functions
+                 'clojure-font-lock-extend-region-comment t)
+    (make-local-variable 'clojure-font-lock-keywords)
+    (add-to-list 'clojure-font-lock-keywords
+                 'clojure-font-lock-mark-comment t)
+    (set (make-local-variable 'open-paren-in-column-0-is-defun-start) nil))
+ 
+  (setq font-lock-defaults
+        '(clojure-font-lock-keywords    ; keywords
+          nil nil
+          (("+-*/.<>=!?$%_&~^:@" . "w")) ; syntax alist
+          nil
+          (font-lock-mark-block-function . mark-defun)
+          (font-lock-syntactic-face-function
+           . lisp-font-lock-syntactic-face-function))))
+ 
+(add-hook 'slime-repl-mode-hook
+          (lambda ()
+            ;(font-lock-mode nil)
+            (clojure-font-lock-setup)
+            ;(font-lock-mode t)
+            ))
+ 
+(defadvice slime-repl-emit (after sr-emit-ad activate)
+  (with-current-buffer (slime-output-buffer)
+    (add-text-properties slime-output-start slime-output-end
+                         '(font-lock-face slime-repl-output-face
+                                          rear-nonsticky (font-lock-face)))))
+ 
+(defadvice slime-repl-insert-prompt (after sr-prompt-ad activate)
+  (with-current-buffer (slime-output-buffer)
+    (let ((inhibit-read-only t))
+      (add-text-properties slime-repl-prompt-start-mark (point-max)
+                           '(font-lock-face slime-repl-prompt-face
+                                            rear-nonsticky
+                                            (slime-repl-prompt
+                                             read-only
+                                             font-lock-face
+                                             intangible))))))
