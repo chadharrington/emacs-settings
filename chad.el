482c9cf50beec70c090d69821144ba7424f2a365
chadharrington <chad.harrington@gmail.com>
2012-03-24 21:39:16 -0600

    Added packages to autoload

--- chad.el
+++ chad.el
@@ -106,3 +106,16 @@
 (add-to-list 'package-archives
              '("marmalade" . 
                "http://marmalade-repo.org/packages/") t)
+
+;; Autoload particular packages that I want if they aren't loaded
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+
