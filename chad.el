3c6380854966dda1c4e124148712d410a7be9e22
Chad Harrington <chad@deercreeklabs.com>
2012-09-24 22:31:42 -0600

    Added configs for nrepl; removed old SLIME keybinding

--- chad.el
+++ chad.el
@@ -43,7 +43,15 @@
       (append '(("\\.rst$" . rst-mode)
                 ("\\.rest$" . rst-mode)) auto-mode-alist))
 
-(global-set-key (kbd "C-x C-z") 'slime-eval-buffer)
+;; Configure nrepl.el
+; Enable eldoc in clojure buffers
+(add-hook 'nrepl-interaction-mode-hook
+          'nrepl-turn-on-eldoc-mode)
+; Stop the error buffer from popping up while working in the REPL buffer
+(setq nrepl-popup-stacktraces nil)
+; Make C-c C-z switch to the *nrepl* buffer in the current window:
+(add-to-list 'same-window-buffer-names "*nrepl*") 
+
 
 
 
