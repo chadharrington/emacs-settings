ddc8a6568dbfcd0f54602b8352070298a0fdb93e
Chad Harrington <chad@deercreeklabs.com>
2014-09-23 14:57:10 -0600

    Manually install cider so I can control the version

--- init.el
+++ init.el
@@ -8,13 +8,17 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-;; Add in your own as you wish:
 (defvar my-packages '(starter-kit starter-kit-lisp starter-kit-eshell
                                   starter-kit-js starter-kit-bindings
-                                  ace-jump-mode cider coffee-mode
+                                  ace-jump-mode coffee-mode
                                   rainbow-delimiters rst switch-window)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
+
+;; Manually add cider so I can control the version
+(add-to-list 'load-path "/Users/chad/.emacs.d/vendor/cider-0.7.0")
+(require 'cider)
+
